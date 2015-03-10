2bc97fc450020088aca307e8eed20bb99a133829
Risang Pratama <risangpratama13@gmail.com>
2015-03-11 06:24:54 +0700

    SLI LIPI Maret 2015

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 date_default_timezone_set("Asia/Jakarta");
 
 class Auth extends CI_Controller {
-
+    
     function __construct() {
         parent::__construct();
         $this->load->model('member');
